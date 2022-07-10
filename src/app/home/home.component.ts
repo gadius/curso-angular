ee07ac1e883217807e14d534c62d1211b66cae71
Gadius <gadlapaz@hotmail.com>
2022-07-10 16:00:48 -0600

    176 custom observer and observable demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +9,32 @@ import { interval, Subscription } from 'rxjs';
 export class HomeComponent implements OnInit, OnDestroy {
   private firstObsSubscription: Subscription;
   constructor() { }
-  
+
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {console.log(count)});
+     /* this.firstObsSubscription = interval(1000).subscribe(count => {console.log(count)}); */
+    const customIntervalObservable = Observable.create( observer => {
+      let count = 0;
+      setInterval( () => {
+        if(count === 5)
+          observer.complete();
+        if(count > 3)
+          observer.error(new Error('Count is greater 3!'));
+
+        count++;
+
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe( data => {
+      console.log(data)
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('Completed');
+    }
+    );
+
   }
 
   ngOnDestroy(): void {
