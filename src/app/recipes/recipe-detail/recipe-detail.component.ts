8bb00c35a6fc87b89e03cb15961703c46185a640
Gadius <gadlapaz@hotmail.com>
2022-07-10 14:18:08 -0600

    163- passing dynamic data

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -20,6 +20,8 @@ export class RecipeDetailComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.recipe = this.recipeService.getRecipe(this.id);
+
+      console.log(this.recipe,this.id);
     })
   }
 
