6631d7e42e7231454321d60237f64ba90999a09d
Gadius <gadlapaz@hotmail.com>
2022-07-11 10:55:59 -0600

    199 TD- Setting and patching Form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,38 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
-  genders = ['male','female'];
+  genders = ['male', 'female'];
   suggestUserName() {
+
     const suggestedName = 'Superuser';
+    /* THIS REPLACES ALL FORM
+    this.signupForm.setValue({
+      userData: { username: suggestedName, email: '' },
+      questionAnswer: '',
+      gender: 'male',
+      secret:''
+    });
+    */
+   //this updates specific input on form
+    this.signupForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    });
   }
 
-    /*
+  /*
   onSubmit(f: NgForm){
     console.log(f);
   }
   */
-  onSubmit(){
-  console.log(this.signupForm);
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
