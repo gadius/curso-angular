743184e451881896d70447d99da154c7417b67b8
Gadius <gadlapaz@hotmail.com>
2022-07-11 08:29:48 -0600

    181- rollback to course-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  ngOnInit() {}
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
