fef86ae1a1ad9fee362265e61230373959c9788a
Gadius <gadlapaz@hotmail.com>
2022-07-11 12:30:16 -0600

    203- reactive setup src code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f') signupForm: NgForm;
-  subscription_default_option = 'advanced';
-  user = {
-    email: '',
-    subscription: '',
-    password:'',
-  };
-
-  submitted : boolean = false;
-
-  onSubmit() {
-    console.log(this.signupForm);
-    this.submitted = true;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.subscription = this.signupForm.value.userData.subscription;
-    this.user.password = this.signupForm.value.password;
-
-    this.signupForm.reset();
-  }
+  genders = ['male', 'female'];
 }
