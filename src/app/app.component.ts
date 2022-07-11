2f12f814423eea19d62fbd5ddc3ca41542d4e228
Gadius <gadlapaz@hotmail.com>
2022-07-11 12:07:16 -0600

    assigntment 6 Template-Drive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,53 +7,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = 'teacher';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer:'',
-    gender:'',
 
+  @ViewChild('f') signupForm: NgForm;
+  subscription_default_option = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password:'',
   };
+
   submitted : boolean = false;
 
-
-  suggestUserName() {
-
-    const suggestedName = 'Superuser';
-    /* THIS REPLACES ALL FORM
-    this.signupForm.setValue({
-      userData: { username: suggestedName, email: '' },
-      questionAnswer: '',
-      gender: 'male',
-      secret:''
-    });
-    */
-   //this updates specific input on form
-    this.signupForm.form.patchValue({
-      userData:{
-        username:suggestedName
-      }
-    });
-  }
-
-  /*
-  onSubmit(f: NgForm){
-    console.log(f);
-  }
-  */
   onSubmit() {
     console.log(this.signupForm);
     this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
+    this.user.subscription = this.signupForm.value.userData.subscription;
+    this.user.password = this.signupForm.value.password;
 
     this.signupForm.reset();
   }
