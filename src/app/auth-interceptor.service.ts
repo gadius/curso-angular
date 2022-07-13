94026630bc08f24d386dd197e79e56fa8d80d4d7
Gadius <gadlapaz@hotmail.com>
2022-07-13 12:29:05 -0600

    275 Response interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpRequest, HttpInterceptor, HttpHandler } from "@angular/common/http";
+import { HttpRequest, HttpInterceptor, HttpHandler, HttpEventType } from "@angular/common/http";
+import { tap } from 'rxjs/operator';
 
 export class AuthInterceptorService{
   intercept(req: HttpRequest<any>, next: HttpHandler){
@@ -6,6 +7,13 @@ export class AuthInterceptorService{
     const modifiedRequest = req.clone({headers: req.headers.append('Auth','xyz')});
 
     //return next.handle(req); //Its like laravel middleware
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap(event => {
+        console.log(event);
+        if(event.type == HttpEventType.Response)
+          console.log('Response arrived, body: data ');
+          console.log(event.body);
+      })
+    );
   }
 }
