095397f7d9ee8fe696a43cc4c80788afdb43cbb1
Gadius <gadlapaz@hotmail.com>
2022-07-13 12:25:32 -0600

    274- Manipulatin request objects using Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,9 @@ import { HttpRequest, HttpInterceptor, HttpHandler } from "@angular/common/http"
 export class AuthInterceptorService{
   intercept(req: HttpRequest<any>, next: HttpHandler){
     console.log("Request is on its way");
-    return next.handle(req); //Its like laravel middleware
+    const modifiedRequest = req.clone({headers: req.headers.append('Auth','xyz')});
+
+    //return next.handle(req); //Its like laravel middleware
+    return next.handle(modifiedRequest);
   }
 }
