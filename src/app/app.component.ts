9749c9f9fbccc0950e509e241c1afec64ed0fa5a
Gadius <gadlapaz@hotmail.com>
2022-07-19 10:32:53 -0600

    SRC code complete Unit Testing introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AuthService } from './auth/auth.service';
-import { LoggingService } from './logging.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(
-    private authService: AuthService,
-    private loggingService: LoggingService
-  ) {}
-
-  ngOnInit() {
-    this.authService.autoLogin();
-    this.loggingService.printLog('Hello from AppComponent ngOnInit');
-  }
+export class AppComponent {
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule,
-    CoreModule
+    FormsModule,
   ],
-  bootstrap: [AppComponent],
-  // providers: [LoggingService]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
